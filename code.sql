c0f3aed9cde36e07756b862360285aed89eecce3
aashiiguptaa23-dev <aashiiguptaa23@gmail.com>
2026-03-10 11:46:14 +0530

    Create Employees table with sample data and query
    Created Employees table and inserted sample data. Added a query to join employees based on their manager.

---
+++ code.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Employees (
+emp_id INT PRIMARY KEY,
+emp_name VARCHAR(50),
+manager_id INT,
+department VARCHAR(50),
+salary INT
+);
+
+INSERT INTO Employees VALUES (1,'Amit',NULL,'Management',120000);
+INSERT INTO Employees VALUES (2,'Ravi',1,'Engineering',80000);
+INSERT INTO Employees VALUES (3,'Neha',1,'Engineering',82000);
+INSERT INTO Employees VALUES (4,'Karan',2,'Engineering',60000);
+INSERT INTO Employees VALUES (5,'Simran',2,'Engineering',62000);
+INSERT INTO Employees VALUES (6,'Pooja',3,'Engineering',61000);
+INSERT INTO Employees VALUES (7,'Rahul',3,'Engineering',64000);
+INSERT INTO Employees VALUES (8,'Arjun',1,'HR',70000);
+
+SELECT 
+e1.manager_id,
+e1.emp_name AS employee_1,
+e2.emp_name AS employee_2
+FROM Employees e1
+JOIN Employees e2
+ON e1.manager_id = e2.manager_id
+AND e1.emp_id < e2.emp_id
+WHERE e1.manager_id IS NOT NULL;
